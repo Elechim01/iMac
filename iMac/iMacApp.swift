ad9d0f27c0cbd66f2a9f9b5940500fb88dd62ac8
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-03 22:19:21 +0200

    Initial Commit

---
+++ iMac/iMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMacApp.swift
+//  iMac
+//
+//  Created by Michele Manniello on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct iMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
